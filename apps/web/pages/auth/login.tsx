e0bee0e6d7e33eaa06d8b9e1e8ce9ee82d8e151f
Nicholas <contactnicholas81@gmail.com>
2022-06-28 21:42:55 +0100

    (web): modify signup page and add login page

--- apps/web/pages/auth/login.tsx
+++ apps/web/pages/auth/login.tsx
@@ -7,20 +7,13 @@ import {
 } from "@splashsaver/ui";
 import { useState } from "react";
 
-const SignUp = () => {
-  const [name, setName] = useState("");
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const signupSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (password.length < 8) {
-      return setError("Password length must be at least 8 characters.");
-    }
-
-    setError("");
   };
 
   return (
@@ -28,27 +21,13 @@ const SignUp = () => {
       <div className="flex-1 h-full"></div>
       <form
         onSubmit={signupSubmitHandler}
-        className="flex flex-col justify-center border-l pl-6 border-stone-800 ml-auto mr-12 w-96 rounded"
+        className="flex flex-col justify-center border-l pl-6 border-stone-800 ml-auto mr-12 w-96"
       >
         <p className="text-red-500 text-xs text-center mb-5">
           {error && error}
         </p>
-        <h1 className="font-bold text-xl mb-2 text-white">Sign Up</h1>
-        <p className="text-gray-400 text-sm mb-4 max-w-xs">
-          Sign up today and start using splashsaver to manage your teams.
-        </p>
-        <InputWrapper>
-          <Label>Name</Label>
-          <Input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            required={true}
-            placeholder="John Doe"
-            onChange={(e) => setName(e.currentTarget.value)}
-          />
-        </InputWrapper>
+        <h1 className="font-bold text-xl mb-2 text-white">Login</h1>
+        <p className="text-gray-400 text-sm mb-4 max-w-xs">Welcome back!</p>
         <InputWrapper>
           <Label>Email</Label>
           <Input
@@ -62,7 +41,7 @@ const SignUp = () => {
           />
         </InputWrapper>
         <InputWrapper>
-          <Label>Password (8 characters)</Label>
+          <Label>Password</Label>
           <Input
             type="password"
             name="password"
@@ -73,18 +52,7 @@ const SignUp = () => {
             onChange={(e) => setPassword(e.currentTarget.value)}
           />
         </InputWrapper>
-        <Button>Sign up for free</Button>
-        <div className="mt-4 text-gray-400 text-xs max-w-xs">
-          By signing up, you agree to our{" "}
-          <Link className="hover:underline text-xs" href="/terms">
-            Terms of Service
-          </Link>{" "}
-          and{" "}
-          <Link className="hover:underline text-xs" href="/privacy">
-            Privacy Policy
-          </Link>
-          .
-        </div>
+        <Button>Login</Button>
       </form>
     </Layout>
   );
@@ -94,4 +62,4 @@ const InputWrapper = ({ children }: { children?: React.ReactNode }) => {
   return <div className="flex flex-col mb-4">{children}</div>;
 };
 
-export default SignUp;
+export default Login;
